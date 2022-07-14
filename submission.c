f9c47d149c2fa9a949989105f01b3a6d3646d253
matallow <akoegpjmdm@icloud.com>
2022-07-14 13:26:54 +0900

    HoT: initial revision

---
+++ submission.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  int i;
+  int tail=0;
+  int head=0;
+  int p;
+  printf("Tossing a coin...\n");
+  for(i=0;i<3;i++)
+  {
+    p=rand()%2;
+    if(p==1)
+    {
+      printf("Round%d: Heads\n",i+1);
+      head++;
+    }
+    else{
+      tail++;
+      printf("Round%d: Tails\n",i+1);
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",head,tail);
+}
